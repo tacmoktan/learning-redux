fbcf8265bcbb5f46ea174a960081b5a9c1115a3f
iamtashi <tacmoktan@gmail.com>
2019-09-26 16:46:28 +0545

    Add routes for component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 //component
 import Blogs from './components/Blogs';
+import Post from './components/Post'; //single post
 
 function App() {
   return (
-    <div className="App">
-      <Blogs />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Switch>
+          <Route exact path="/" component={Blogs} />
+          <Route path="/post/:id" component={Post}/> 
+        </Switch>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
