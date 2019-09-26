0a9af8a6dc1cd87ba80665a1722e2ec3a6c78796
iamtashi <tacmoktan@gmail.com>
2019-09-26 21:14:02 +0545

    Updates

---
+++ precache-manifest.09f6afd93a6ec77b0220f86d8b3bd13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e639427e389408d56cdad6c33253b5",
+    "url": "/learning-redux/index.html"
+  },
+  {
+    "revision": "eee9a71255944f147875",
+    "url": "/learning-redux/static/css/main.f0129196.chunk.css"
+  },
+  {
+    "revision": "74148ffc6123420b8673",
+    "url": "/learning-redux/static/js/2.5bc7115c.chunk.js"
+  },
+  {
+    "revision": "eee9a71255944f147875",
+    "url": "/learning-redux/static/js/main.aafed4e4.chunk.js"
+  },
+  {
+    "revision": "4deecb695c16134f9988",
+    "url": "/learning-redux/static/js/runtime-main.1e335935.js"
+  }
+]);
